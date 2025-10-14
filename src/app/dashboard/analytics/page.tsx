c12fc30f4d96e5469f4d02331317fb8fb777a1c2
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-10-14 16:35:14 +0100

    fix:treaShaking

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React from 'react';
-import DashboardLayout from '../../../components/Dashboard/DashboardLayout';
 import PropertyAnalytics from '../../../components/Analytics/PropertyAnalytics';
 
 const AnalyticsPage: React.FC = () => {
