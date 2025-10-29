2d46109e5d9530f675ea4153c9e2259a6ff5f583
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-10-29 09:12:48 +0100

    checkPwaNextjs

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,49 @@
-const CACHE_NAME = 'next-pwa-cache-v1';
-const OFFLINE_URL = '/offline';
+// const CACHE_NAME = 'next-pwa-cache-v1';
+// const OFFLINE_URL = '/offline';
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        '/',
-        '/offline',
-        '/manifest.json',
-        '/image/plogo.png',
-        '/image/plogo.png',
-      ]);
-    })
-  );
-});
+// self.addEventListener('install', (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       return cache.addAll([
+//         '/',
+//         '/offline',
+//         '/manifest.json',
+//         '/image/plogo.png',
+//         '/image/plogo.png',
+//       ]);
+//     })
+//   );
+// });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request).catch(() => {
-        return caches.match(OFFLINE_URL);
-      });
-    })
-  );
-});
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request).catch(() => {
+//         return caches.match(OFFLINE_URL);
+//       });
+//     })
+//   );
+// });
+
+self.addEventListener('push', function (event) {
+  if (event.data) {
+    const data = event.data.json()
+    const options = {
+      body: data.body,
+      icon: data.icon || '/image/plogo.png',
+      badge: '/favicon-16x16.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2',
+      },
+    }
+    event.waitUntil(self.registration.showNotification(data.title, options))
+  }
+})
+ 
+self.addEventListener('notificationclick', function (event) {
+  console.log('Notification click received.')
+  event.notification.close()
+  event.waitUntil(clients.openWindow('https://ppoint.vercel.app/'))
+})
